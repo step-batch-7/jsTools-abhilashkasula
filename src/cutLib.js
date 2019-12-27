7cec1a0ad7e7b9b6d1c8ac34daf97265338f2585
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-12-27 11:58:36 +0530

    Renamed colZeroErr to columnZeroErr

--- src/cutLib.js
+++ src/cutLib.js
@@ -1,18 +1,20 @@
 const cutRowOfColumn = function(line) {
+  const rowOfColumn = "";
   if (!line.includes(this.delimiter)) return line;
   const splitLine = line.split(this.delimiter);
-  return splitLine[+this.field - 1] || "";
+  return splitLine[+this.field - 1] || rowOfColumn;
 };
 
 const cutRowsOfColumns = (lines, cutOptions) => {
   let rowsOfColumns = "";
-  const colZeroErr = "cut: [-cf] list: values may not include zero";
+  const error = "";
+  const columnZeroErr = "cut: [-cf] list: values may not include zero";
   const notANumberErr = "cut: [-cf] list: illegal list value";
   const { field } = cutOptions;
-  if (field == 0) return { error: colZeroErr, rowsOfColumns };
+  if (field == 0) return { error: columnZeroErr, rowsOfColumns };
   if (isNaN(+field)) return { error: notANumberErr, rowsOfColumns };
   rowsOfColumns = lines.map(cutRowOfColumn.bind(cutOptions)).join("\n");
-  return { error: "", rowsOfColumns };
+  return { error, rowsOfColumns };
 };
 
 const parseOptions = options => {
